cdea21f029d95f667a934c65fd7f57062c7018b8
Kristina Spasevska <spasevska.kristina87@gmail.com>
2019-02-28 00:12:01 +0100

    Files added

---
+++ Class02-homework/Class02-bonus/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class02_bonus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter degrees celsius: ");
+            int t = int.Parse(Console.ReadLine());
+
+            if (t < 0)
+            {
+                Console.WriteLine("Freezing weather!");
+            }else if ((t > 0) && (t < 10))
+            {
+                Console.WriteLine("Very cold weather!");
+            }else if ((t >= 10) && (t < 20))
+            {
+                Console.WriteLine("Cold weather!");
+            }else if ((t >= 20) && (t < 30))
+            {
+                Console.WriteLine("Normal temperature!");
+            }else if ((t >= 30) && (t < 40))
+            {
+                Console.WriteLine("It's hot!");
+            }else if (t >= 40)
+            {
+                Console.WriteLine("It's very hot!");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Class02-homework/Class02-homework/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class02_homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Find out what is behind door: 1, 2 or 3 by entering the appropriate number!");
+            //byte x = byte.Parse(Console.ReadLine());
+            //switch (x)
+            //{
+            //    case 1:
+            //        Console.WriteLine("You got a new car!");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("You got a new plane!");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("You got a new bike!");
+            //        break;
+            //    default:
+            //        Console.WriteLine("No door with that number!");
+            //        break;
+            //}
+            //Console.ReadLine();
+
+            Console.WriteLine("Enter the first number:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the third number:");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the fourth number:");
+            int d = int.Parse(Console.ReadLine());
+            int res = (a + b + c + d) / 4;
+            Console.WriteLine($"The average of {a}, {b}, {c} and {d} is: {res}");
+            Console.ReadLine();
+        }
+    }
+}
